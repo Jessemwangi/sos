132d237b7e3032ab2ac1233bf668b85bfdb74c70
Anna Petelin <andorjamb@protonmail.com>
2023-03-04 12:35:56 +0200

    add sosbutton and styling

---
+++ src/Components/SOSButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/SOSButton.css';
+
+const SOSButton = () => {
+
+    const sosButton = document.getElementById('sosButton');
+
+    return (
+        <button className="sosButton" id="sosButton" onClick={() => sosButton!.classList.add('flash')}>
+            <span>SOS</span>
+        </button>
+    );
+};
+
+export default SOSButton;
---
+++ src/Components/SOSMenu.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "../styles/SOSMenu.css";
+
+const SOSMenu = () => {
+    return (
+        <div className="sosMenu">
+            <div className="sosMenuButton"></div>
+            <div className="sosMenuButton"></div>
+            <div className="sosMenuButton"></div>
+            <div className="sosMenuButton"></div>
+            <div className="sosMenuButton"></div>
+            <div className="sosMenuButton"></div>
+
+        </div>
+    );
+};
+
+export default SOSMenu;
