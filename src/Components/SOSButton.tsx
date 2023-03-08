eb94978972bd4f2bca0daaf6ae5ce8dc3392e6ae
Anna Petelin <andorjamb@protonmail.com>
2023-03-08 13:26:33 +0200

    reaarange folders

--- src/Components/SOSButton.tsx
+++ src/Components/SOSButton.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import '../styles/SOSButton.css';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { activate } from '../features/sosButtonSlice';
 
 const SOSButton = () => {
-    const active = useSelector((state: any) => state.sosButton.active);
+    //const active = useSelector((state: any) => state.sosButton.active);
     const dispatch = useDispatch();
-
-    console.log(active);
 
     function activeSosButton() {
         const sosButton = document.getElementById('sosButton');
         sosButton!.classList.toggle('flash');
         dispatch(activate());
-
     }
+
+
 
     return (
         <button className="sosButton" id="sosButton" onClick={activeSosButton}>
             <span>SOS</span>
-            <p>status: {active}</p>
         </button>
+
     );
 };
 
